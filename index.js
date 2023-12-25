c475526d4b3928350bc06e92e0a7bff5be5bbba3
umer-ahmed5002 <142247797+umer-ahmed5002@users.noreply.github.com>
2023-12-25 18:45:20 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
+
+
 let cards = document.querySelector("#cards")
+
 
 fetch("https://api.github.com/users")
 .then((data) =>{
@@ -7,7 +10,7 @@ fetch("https://api.github.com/users")
 }).then((user) =>{
     let newHtml = ''
     user.forEach((list) => {
-        console.log(list);
+        // console.log(list);
         newHtml += 
         `
         <span class="card" onclick="redirectToProfile('${list.login}')">
@@ -24,4 +27,45 @@ fetch("https://api.github.com/users")
 function redirectToProfile(username) {
     // Redirect to the profile page for the clicked user
     window.location.href = `profile.html?user=${username}`;
+}
+
+
+function findUser(){
+// api to get user
+    let userInput = document.querySelector("#inputUser")
+    let userValue = userInput.value;
+    
+
+    fetch(`https://api.github.com/users/${userValue}`)
+    .then((data) =>{
+        return data.json()
+    }).then((user) =>{
+        console.log(user);
+        if(user.login){
+            let newHtml = ''
+            let towHtml = ''
+            cards.innerHTML = ''
+             newHtml = 
+             `
+             <span class="card" onclick="redirectToProfile('${user.login}')">
+             <img class="images" src="${user.avatar_url}" alt="">
+             <span class="names"> ${user.login}</span>
+             </span>
+             
+             `
+             cards.innerHTML = newHtml;
+         
+        }else{
+
+            towHtml = 
+            `
+            <p>This is not found</p>
+
+            `
+
+            cards.innerHTML = towHtml;
+
+        }
+      
+        });
 }
